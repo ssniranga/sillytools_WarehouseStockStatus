26c7c99273dae7564b54e41de638d9a17d19df70
niranga_r <niranga@biopak.com.au>
2023-08-29 10:36:03 +1000

    enabled module by default

---
+++ Block/Adminhtml/System/Config/Form/Info.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SillyTools\WarehouseStockStatus\Block\Adminhtml\System\Config\Form;
+
+/**
+ *
+ */
+class Info extends \SillyTools\Core\Block\Adminhtml\System\Config\Form\Info
+{
+
+    /**
+     * Return extension title
+     * @return string
+     */
+    protected function getModuleTitle(): string
+    {
+        return 'Warehouse Stock Status Extension';
+    }
+}
